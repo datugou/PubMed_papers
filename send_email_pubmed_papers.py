cecdb3beac33262bee560f954b161032921c6097
datugou <953274732@qq.com>
2022-09-13 02:21:40 +0800

    Update send_email_pubmed_papers.py

--- send_email_pubmed_papers.py
+++ send_email_pubmed_papers.py
@@ -17,15 +17,11 @@ reldate = 2 #设定只检索最近 7 天发表的文献
 # 需要在 Settings → Secrets → Dependabot → New repository secret 设置邮箱服务器信息
 try:
     mail_host = 'smtp.163.com'
-    mail_host = 'smtp.163.com'
-    mail_user = 'guominjunl'
-    mail_pass = 'IBCEKRHPKLJITQMA'
-    sender = 'guominjunl@163.com' 
-    receivers = ['mjguo@nggtbio.com.cn'] 
-#     mail_user = os.getenv('MAIL_USER').strip()
-#     mail_pass = os.getenv('MAIL_PASS').strip()
-#     sender = os.getenv('SEND_MIAL').strip()
-#     receivers = [os.getenv('RECEIVE_MIAL').strip()]
+    mail_user = os.getenv('MAIL_USER').strip()
+    mail_pass = os.getenv('MAIL_PASS').strip()
+    sender = os.getenv('SEND_MAIL').strip()
+    receivers = [os.getenv('RECEIVE_MAIL').strip()]
+    print(type(mail_user))
 except:
     print('未设置邮箱信息')
 
